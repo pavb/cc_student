2263650d1b39b8f74aada03e2a77bcf81233ca6d
pav-b <pav-b@192.168.0.74>
2020-08-13 16:16:00 +0200

    ...

--- Tag34/src/ml/brainin/Aufgabe_ArrayList.java
+++ Tag34/src/ml/brainin/Aufgabe_ArrayList.java
@@ -12,10 +12,7 @@ public class Aufgabe_ArrayList {
 
 	public static void main(String[] args) {
 		System.out.println("------ A1 -----");
-		List<Integer> listA1 = new ArrayList<>();
-		for (int i = 0; i <20; i++) {
-			listA1.add(i,(int)(Math.random() * (100) + 1));
-		}
+		List<Integer> listA1 = getArray(20, 100);
 		listA1.iterator().forEachRemaining((number)->System.out.print(number+" "));
 		System.out.println();
 
@@ -47,33 +44,19 @@ public class Aufgabe_ArrayList {
 		System.out.println();
 
 		System.out.println("------ A6 -----");
-		List<Integer> listA6 = new ArrayList<>();
-		for (int i = 0; i <20; i++) {
-			listA6.add(i,(int)(Math.random() * (100) + 1));
-		}
+		List<Integer> listA6 = getArray(20, 100);
 		Collections.sort(listA6);
 		listA6.stream().forEach((number)->System.out.print(number+" "));	
 		System.out.println();
 
 		System.out.println("------ A7 -----");
-		List<Integer> listA7 = new ArrayList<>();
-		for (int i = 0; i <10; i++) {
-			listA7.add(i,(int)(Math.random() * (50) + 1));
-		}
-		listA6.stream().forEach((number)->System.out.print(number+" "));
+		List<Integer> listA7 = getArray(10, 50);
 		int index = listA7.indexOf(12);
-		System.out.println();
 		System.out.println(index >=0?("Current index of 12 is : "+index):"Thre is no 12 in the list ");
 		System.out.println();
 
 		System.out.println("------ A8 -----");
-		List<Integer> listA8 = new ArrayList<>();
-		System.out.println("Current list : ");
-		for (int i = 0; i <10; i++) {
-			int number = (int)(Math.random() * (50) + 1);
-			listA8.add(i,number);
-			System.out.print(number+" ");
-		}
+		List<Integer> listA8 = getArray(10, 50);
 		for (int i = listA8.size()-1; i>=0; i--) {
 			int number = listA8.get(i);
 			if (number%2 != 0) {
@@ -85,27 +68,13 @@ public class Aufgabe_ArrayList {
 		System.out.println();
 
 		System.out.println("------ A9 -----");
-		List<Integer> listA9 = new ArrayList<>();
-		System.out.println("Current list : ");
-		for (int i = 0; i <10; i++) {
-			int number = (int)(Math.random() * (50) + 1);
-			listA9.add(i,number);
-			System.out.print(number+" ");
-		}
-		System.out.println();
+		List<Integer> listA9 = getArray(10, 50);
 		listA9.removeIf((number)-> number%2 !=0);
 		listA9.stream().forEach((number)->System.out.print(number+" "));
 		System.out.println();
 	
 		System.out.println("------ A10 -----");
-		List<Integer> listA10 = new ArrayList<>();
-		System.out.println("Current list : ");
-		for (int i = 0; i <10; i++) {
-			int number = (int)(Math.random() * (50) + 1);
-			listA10.add(i,number);
-			System.out.print(number+" ");
-		}
-		System.out.println();
+		List<Integer> listA10= getArray(10, 50);
 		IntStream.range(0, listA10.size()).forEachOrdered(i -> {
 			if (listA10.get(i)%2 !=0) {
 				listA10.set(i,0);
@@ -115,19 +84,23 @@ public class Aufgabe_ArrayList {
 		System.out.println();
 
 		System.out.println("------ A11 -----");
-		List<Integer> listA11 = new ArrayList<>();
-		System.out.println("Current list : ");
-		for (int i = 0; i <10; i++) {
-			int number = (int)(Math.random() * (50) + 1);
-			listA11.add(i,number);
-			System.out.print(number+" ");
-		}
-		System.out.println();
+		List<Integer> listA11 = getArray(10, 50);
 		Integer[] array = listA11.toArray(new Integer[0]);
 		System.out.println("Current array : ");
 		for (int number : array) {
 			System.out.print(number+" ");
 		}
 
+	}
+	private static List<Integer> getArray(int size, int random) {
+		List<Integer> result = new ArrayList<>();
+		System.out.println("Current list : ");
+		for (int i = 0; i <size; i++) {
+			int number = (int)(Math.random() * (random) + 1);
+			result.add(i,number);
+			System.out.print(number+" ");
+		}
+		System.out.println();
+		return result;
 	}
 }
